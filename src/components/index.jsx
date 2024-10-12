0ee2e73cc0ea9b798d67cbe1bee3c4326c497b86
Your Name <yousaf.8666305@gmail.com>
2024-10-13 00:12:03 +0500

    new changes and make some files and folders

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import Header from "./Headers/Headers";
+import Footer from "./Footers/Footers";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from './Headers/LogoutBtn'
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
