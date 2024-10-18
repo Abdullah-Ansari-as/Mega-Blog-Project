097f309b2a0bd6706772c448339145fe7bc04d6c
Your Name <yousaf.8666305@gmail.com>
2024-10-18 14:51:14 +0500

    some changes

--- src/components/Headers/LogoutBtn.jsx
+++ src/components/Headers/LogoutBtn.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import {useDispatch} from 'react-redux'
 import authService from '../../appwirte/auth'
 import {logout} from '../../store/authSlice'
 
 function LogoutBtn() {
-
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
     const logoutHandler = () => {
-        authService.logout.then(() => {
+        authService.logout().then(() => {
             dispatch(logout())
         })
     }
-
-
   return (
     <button
     className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
